79dfd47d54226945bbb209d41636935a4e26b568
jmirancid <jmirancid@gmail.com>
2015-05-11 00:25:06 -0300

    Agregado nuevo helper para enums.

--- Framework.MVC/Extensions/HtmlHelperExtensions.cs
+++ Framework.MVC/Extensions/HtmlHelperExtensions.cs
@@ -2,33 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
 namespace Framework.MVC.Extensions
 {
-	public static class HtmlHelperExtensions
-	{
-		public static MvcHtmlString DisplayNameFor<TModel, TProperty>(
-			this HtmlHelper<IEnumerable<TModel>> htmlHelper, Expression<Func<TModel, TProperty>> expression)
-		{
-			return DisplayNameFor(expression);
-		}
+    public static class HtmlHelperExtensions
+    {
+        public static MvcHtmlString DisplayNameFor<TModel, TProperty>(
+            this HtmlHelper<IEnumerable<TModel>> htmlHelper, Expression<Func<TModel, TProperty>> expression)
+        {
+            return DisplayNameFor(expression);
+        }
 
-		public static MvcHtmlString DisplayNameFor<TModel, TProperty>(
-			this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression)
-		{
-			return DisplayNameFor(expression);
-		}
+        public static MvcHtmlString DisplayNameFor<TModel, TProperty>(
+            this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression)
+        {
+            return DisplayNameFor(expression);
+        }
 
-		private static MvcHtmlString DisplayNameFor<TModel, TProperty>(Expression<Func<TModel, TProperty>> expression)
-		{
-			var metadata = ModelMetadata.FromLambdaExpression<TModel, TProperty>(expression, new ViewDataDictionary<TModel>());
-			var htmlFieldName = ExpressionHelper.GetExpressionText(expression);
-			string s = metadata.DisplayName ?? (metadata.PropertyName ?? htmlFieldName.Split(new char[] { '.' }).Last<string>());
+        private static MvcHtmlString DisplayNameFor<TModel, TProperty>(Expression<Func<TModel, TProperty>> expression)
+        {
+            var metadata = ModelMetadata.FromLambdaExpression<TModel, TProperty>(expression, new ViewDataDictionary<TModel>());
+            var htmlFieldName = ExpressionHelper.GetExpressionText(expression);
+            string s = metadata.DisplayName ?? (metadata.PropertyName ?? htmlFieldName.Split(new char[] { '.' }).Last<string>());
 
-			return new MvcHtmlString(HttpUtility.HtmlEncode(s));
-		}
-	}
+            return new MvcHtmlString(HttpUtility.HtmlEncode(s));
+        }
+
+        public static MvcHtmlString DisplayEnumFor<TModel>(
+            this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, int>> expression, Type enumType)
+        {
+            var value = (int)ModelMetadata.FromLambdaExpression(expression, htmlHelper.ViewData).Model;
+            string enumValue = Enum.GetName(enumType, value);
+
+            return new MvcHtmlString(htmlHelper.Encode(enumValue));
+        }
+    }
 }
