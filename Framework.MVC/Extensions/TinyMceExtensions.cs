cb0d8a4543ff6618b654d92931a8b64dad291114
jmirancid <jmirancid@gmail.com>
2016-04-18 01:38:45 -0300

    Agregado modo editor ReadOnly.

--- Framework.MVC/Extensions/TinyMceExtensions.cs
+++ Framework.MVC/Extensions/TinyMceExtensions.cs
@@ -121,14 +121,15 @@ namespace Framework.MVC.Extensions
                     theme = "modern",
                     plugins = new string[]
                         {
-                            "advlist autolink lists link image charmap print preview anchor",
-                            "searchreplace visualblocks code fullscreen",
-                            "insertdatetime media table contextmenu paste",
-                            "textcolor colorpicker "
+                            "code contextmenu paste"
                         },
                     force_p_newlines = false,
                     forced_root_block = false,
-                    toolbar = "insertfile undo redo | styleselect | bold italic forecolor backcolor | alignleft aligncenter alignright alignjustify | table | bullist numlist outdent indent | link image"
+                    toolbar = "undo redo | cut,copy,paste | bold italic underline | code",
+                    theme_advanced_toolbar_location = "top",
+                    theme_advanced_toolbar_align = "left",
+                    theme_advanced_statusbar_location = "bottom",
+                    theme_advanced_resizing = true
                 });
             }
         }
@@ -155,6 +156,34 @@ namespace Framework.MVC.Extensions
                     force_p_newlines = false,
                     forced_root_block = false,
                     toolbar = "insertfile undo redo | styleselect | bold italic forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent"
+                });
+            }
+        }
+
+        public static IDictionary<string, object> ReadOnly
+        {
+            get
+            {
+                return new RouteValueDictionary(new
+                {
+                    menubar = false,
+                    language = "en",
+                    mode = "specific_textareas",
+                    editor_selector = "mceEditor",
+                    width = "550px",
+                    theme = "modern",
+                    @readonly = 1,
+                    plugins = new string[]
+                        {
+                            "code contextmenu paste"
+                        },
+                    force_p_newlines = false,
+                    forced_root_block = false,
+                    toolbar = "undo redo | cut,copy,paste | bold italic underline | code",
+                    theme_advanced_toolbar_location = "top",
+                    theme_advanced_toolbar_align = "left",
+                    theme_advanced_statusbar_location = "bottom",
+                    theme_advanced_resizing = true
                 });
             }
         }
