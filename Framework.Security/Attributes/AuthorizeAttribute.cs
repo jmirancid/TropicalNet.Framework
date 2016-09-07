72897123f383cc64aced003b974d91eac0057a00
jmirancid <jmirancid@gmail.com>
2016-09-07 18:52:04 -0300

    Agregado nuevo proyecto Framework.Security. Quitado attributo de seguridad en Framework.Mvc.

--- Framework.Security/Attributes/AuthorizeAttribute.cs
+++ Framework.Security/Attributes/AuthorizeAttribute.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Web.Mvc;
 
-namespace Framework.MVC.Attributes
+namespace Framework.Security.Attributes
 {
     public class AuthorizeAttribute :
             System.Web.Mvc.AuthorizeAttribute
